db94529b6aa2806bf687ed861c8cc306d702090c
Laurence Carew <ltcarew@gmail.com>
2026-05-19 13:33:40 -0700

    added nav and bulma

--- caregiver_registry/config/urls.py
+++ caregiver_registry/config/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.views import LogoutView
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("registry.urls")),
     path("",include("django.contrib.auth.urls")),
+    path(
+        "logout/",
+        LogoutView.as_view(),
+        name="logout"
+    ),
 ]
